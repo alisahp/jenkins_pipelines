860bbe5ff0640ced2ae7e0ca3983d02b06e807ec
Ali <alisevhp@gmail.com>
2020-02-08 11:11:25 -0600

    as

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,16 @@
 node {
-    properties([parameters([choice(choices: ['us-east-1', 'us-west-1'], description: 'add region', name: 'AMI_REGION')])])
-
+    properties([parameters([choice(choices: ['us-east-1', 'us-west-1', 'us-west-2'], description: 'please select a region', name: 'ami_region')])])
     stage ("Pull Repo"){
-        git 'git@github.com:farrukh90/packer.git'
-
-}
+        git 'https://github.com/enver0173/packer.git'
+    }
     stage ("Build Image"){
         sh "packer version"
         sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
-
-}
+    }
     stage ("Send Notification to Slack"){
         echo "Hello"
-
-}
+    }
     stage ("Send Email"){
-        echo "Hello"
-
+        echo "Hellp"
+    }
 }
