56a07af99763083b9bd256a2dfc232f59a29cfa9
Ali <alisevhp@gmail.com>
2020-02-08 11:35:51 -0600

    two choice added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,15 @@
 node {
-    properties([parameters([choice(choices: ['us-east-1', 'us-west-1', 'us-west-2'], description: 'please select a region', name: 'ami_region')])])
+    properties([parameters([
+        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'), 
+        choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose a region', name: 'AMI_REGION')])])
+
+
     stage ("Pull Repo"){
-        git 'https://github.com/enver0173/packer.git'
+        git 'git@github.com:farrukh90/packer.git'
     }
     stage ("Build Image"){
         sh "packer version"
-        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
+        //sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
     }
     stage ("Send Notification to Slack"){
         slackSend channel: 'nagios_alerts', message: 'Golden AMI has been built'
