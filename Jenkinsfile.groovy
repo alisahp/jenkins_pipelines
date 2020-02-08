ce773d4dfcce6a7a2fef23aef2b79b07d223e82f
Ali <alisevhp@gmail.com>
2020-02-08 11:01:36 -0600

    packer build added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,11 +2,12 @@ node {
     properties([parameters([choice(choices: ['us-east-1', 'us-west-1'], description: 'add region', name: 'AMI_REGION')])])
 
     stage ("Pull Repo"){
-        echo "hello"
+        git 'git@github.com:farrukh90/packer.git'
 
 }
     stage ("Build Image"){
         sh "packer version"
+        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json
 
 }
     stage ("Send Notification to Slack"){
