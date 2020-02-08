4d0e1002b5be899d50494d5138c0c3d1bf080b4b
Ali <alisevhp@gmail.com>
2020-02-08 10:33:05 -0600

    packer jenkins tasks added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,17 @@
+node {
+    stage ("Pull Repo"){
+        echo "hello"
 
+}
+    stage ("Build Image"){
+        sh "packer version"
+
+}
+    stage ("Send Notification to Slack"){
+        echo "Hello"
+
+}
+    stage ("Send Email"){
+        echo "Hello"
+
+}
