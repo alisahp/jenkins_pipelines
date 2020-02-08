3e69eca35b1c4df2324a4a55c12d011285630b08
Ali <alisevhp@gmail.com>
2020-02-08 13:33:14 -0600

    as

--- jenkinsfile_for_EKS.groovy
+++ jenkinsfile_for_EKS.groovy
@@ -3,13 +3,19 @@ node{
         git 'https://github.com/alisahp/terraform-iaac-eks-burak.git'
     }
     stage("Download Terraform"){
-        sh "terraform version"
-        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
-        sh "unzip terraform_0.12.19_linux_amd64.zip"
-        sh "./terraform version"
+        ws {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
-    stage("stage1"){
-        echo "Hello"
+    stage("Set Backend"){
+        ws ("terraform-iaac-eks-burak"){
+            pwd()
+            sh "ls -l"
+            sh "ls -l ../"
+            //sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+        }
     }
     stage("stage1"){
         echo "Hello"
